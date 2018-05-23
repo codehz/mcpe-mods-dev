47a8cd3e7e6af3ef3d430b9cf72321775c0e79cc
CodeHz <CodeHz@outlook.com>
2018-05-23 15:33:33 +0800

    Blacklist mod

---
+++ src/blacklist/main.cpp
@@ -0,0 +1,243 @@
+#include <string>
+#include <cstdlib>
+#include <sstream>
+#include <climits>
+#include <cstdio>
+#include <ios>
+#include <iomanip>
+#include <iostream>
+
+#include <minecraft/command/Command.h>
+#include <minecraft/command/CommandMessage.h>
+#include <minecraft/command/CommandOutput.h>
+#include <minecraft/command/CommandParameterData.h>
+#include <minecraft/command/CommandRegistry.h>
+#include <minecraft/command/CommandVersion.h>
+
+template <typename T>
+bool isValid(T const &t)
+{
+  union {
+    unsigned const *x;
+    T const *tr;
+  };
+  tr = &t;
+  return *x;
+}
+
+template <typename T>
+std::string stringify(T const &t)
+{
+  std::stringstream ss;
+  ss << t;
+  return ss.str();
+}
+
+struct UUID
+{
+  short p2, p1;
+  unsigned p0, pr;
+  short p4, p3;
+
+  friend std::ostream &operator<<(std::ostream &os, const UUID &uuid)
+  {
+    os << std::hex << std::setfill('0') << std::setw(8)
+       << uuid.p0 << '-' << std::setw(4)
+       << uuid.p1 << '-'
+       << uuid.p2 << '-'
+       << uuid.p3 << '-'
+       << uuid.p4 << std::setw(8) << uuid.pr;
+    return os;
+  }
+};
+
+UUID getUUID(std::string str)
+{
+  UUID uuid;
+  const char *cstr = str.c_str();
+  uuid.p0 = std::strtoul(cstr, 0, 16);
+  uuid.p1 = std::strtoul(cstr + 9, 0, 16);
+  uuid.p2 = std::strtoul(cstr + 14, 0, 16);
+  uuid.p3 = std::strtoul(cstr + 19, 0, 16);
+  uuid.p4 = std::strtoul(str.substr(24, 4).c_str(), 0, 16);
+  uuid.pr = std::strtoul(cstr + 28, 0, 16);
+  return uuid;
+}
+
+struct Entity
+{
+  char fillerZ[44];
+  char fillerX[0x117c];
+  UUID uuid;
+  char fillerY[0x142c - 0x117c - sizeof(UUID)];
+  void *id;
+  const std::string &getNameTag() const;
+};
+
+struct Player : Entity
+{
+};
+
+template <typename T>
+struct CommandSelectorResults
+{
+  std::shared_ptr<std::vector<T *>> content;
+  bool empty() const;
+};
+
+struct CommandSelectorBase
+{
+  CommandSelectorBase(bool);
+  virtual ~CommandSelectorBase();
+};
+
+template <typename T>
+struct CommandSelector : CommandSelectorBase
+{
+  char filler[0x74];
+  CommandSelector();
+
+  const CommandSelectorResults<T> results(CommandOrigin const &) const;
+};
+
+struct CommandSelectorPlayer : CommandSelector<Player>
+{
+  CommandSelectorPlayer() : CommandSelector() {}
+  ~CommandSelectorPlayer() {}
+
+  static typeid_t<CommandRegistry> type_id()
+  {
+    static typeid_t<CommandRegistry> ret = type_id_minecraft_symbol<CommandRegistry>("_ZZ7type_idI15CommandRegistry15CommandSelectorI6PlayerEE8typeid_tIT_EvE2id");
+    return ret;
+  }
+};
+
+extern "C" void kickPlayer(Player *p);
+extern "C" void banPlayer(void *uuid, const std::string &str);
+extern "C" void pardonPlayer(void *uuid, const std::string &str);
+
+struct BanCommand : Command
+{
+  CommandSelectorPlayer target;
+  ~BanCommand() override = default;
+  static void setup(CommandRegistry &registry)
+  {
+    registry.registerCommand("ban", "Ban player", (CommandPermissionLevel)3, (CommandFlag)0, (CommandFlag)0);
+    registry.registerOverload<BanCommand>("ban", CommandVersion(1, INT_MAX),
+                                          CommandParameterData(CommandSelectorPlayer::type_id(), &CommandRegistry::parse<CommandSelector<Player>>, "target", (CommandParameterDataType)0, nullptr, offsetof(BanCommand, target), false, -1));
+  }
+
+  void execute(CommandOrigin const &origin, CommandOutput &outp) override
+  {
+    auto res = target.results(origin);
+    if (!res.empty())
+    {
+      for (auto &ent : *res.content)
+      {
+        banPlayer(&ent->uuid, "Banned");
+        kickPlayer(ent);
+        outp.addMessage("§4[Blacklist Mod] Baned " + stringify(ent->uuid));
+      }
+    }
+    outp.success();
+  }
+};
+
+struct BanUUIDCommand : Command
+{
+  CommandMessage uuid;
+  ~BanUUIDCommand() override = default;
+  static void setup(CommandRegistry &registry)
+  {
+    registry.registerCommand("banuuid", "Ban player by UUID", (CommandPermissionLevel)3, (CommandFlag)0, (CommandFlag)0);
+    registry.registerOverload<BanUUIDCommand>("banuuid", CommandVersion(1, INT_MAX),
+                                              CommandParameterData(CommandMessage::type_id(), &CommandRegistry::parse<CommandMessage>, "uuid", (CommandParameterDataType)0, nullptr, offsetof(BanUUIDCommand, uuid), false, -1));
+  }
+
+  void execute(CommandOrigin const &origin, CommandOutput &outp) override
+  {
+    if (isValid(uuid))
+    {
+      auto str = uuid.getMessage(origin);
+      if (str.length() == 36) {
+        UUID uuid = getUUID(str);
+        banPlayer(&uuid, "Banned");
+        outp.addMessage("§4[Blacklist Mod] Banned " + str);
+      }
+    }
+    outp.success();
+  }
+};
+
+struct PardonCommand : Command {
+  CommandMessage uuid;
+  ~PardonCommand() override = default;
+  static void setup(CommandRegistry &registry)
+  {
+    registry.registerCommand("pardon", "Pardon player by UUID", (CommandPermissionLevel)3, (CommandFlag)0, (CommandFlag)0);
+    registry.registerOverload<PardonCommand>("pardon", CommandVersion(1, INT_MAX),
+                                              CommandParameterData(CommandMessage::type_id(), &CommandRegistry::parse<CommandMessage>, "pardon", (CommandParameterDataType)0, nullptr, offsetof(PardonCommand, uuid), false, -1));
+  }
+
+  void execute(CommandOrigin const &origin, CommandOutput &outp) override
+  {
+    if (isValid(uuid))
+    {
+      auto str = uuid.getMessage(origin);
+      if (str.length() == 36) {
+        UUID uuid = getUUID(str);
+        pardonPlayer(&uuid, "Banned");
+        outp.addMessage("§2[Blacklist Mod] Pardoned " + str);
+      }
+    }
+    outp.success();
+  }
+};
+
+struct KickCommand : Command
+{
+  CommandSelectorPlayer target;
+  ~KickCommand() override = default;
+  static void setup(CommandRegistry &registry)
+  {
+    registry.registerCommand("kick", "Kick player", (CommandPermissionLevel)3, (CommandFlag)0, (CommandFlag)0);
+    registry.registerOverload<KickCommand>("kick", CommandVersion(1, INT_MAX),
+                                           CommandParameterData(CommandSelectorPlayer::type_id(), &CommandRegistry::parse<CommandSelector<Player>>, "target", (CommandParameterDataType)0, nullptr, offsetof(KickCommand, target), false, -1));
+  }
+
+  void execute(CommandOrigin const &origin, CommandOutput &outp) override
+  {
+    auto res = target.results(origin);
+    if (!res.empty())
+    {
+      for (auto &ent : *res.content)
+      {
+        kickPlayer(ent);
+        outp.addMessage("§4[Blacklist Mod] Kicked " + stringify(ent->uuid));
+      }
+    }
+    outp.success();
+  }
+};
+
+extern "C"
+{
+  UUID *createUUID(const char* str) {
+    static UUID uuid;
+    uuid = getUUID(str);
+    return &uuid;
+  }
+
+  std::string *bannedStr() {
+    static std::string str = "Banned";
+    return &str;
+  }
+
+  void setupCommands(CommandRegistry &registry)
+  {
+    BanCommand::setup(registry);
+    BanUUIDCommand::setup(registry);
+    PardonCommand::setup(registry);
+    KickCommand::setup(registry);
+  }
+}
---
+++ src/blacklist/main.nim
@@ -0,0 +1,117 @@
+import pub.hook, pub.cppstring, strformat, strutils, os, sets, streams
+const ModBase {.strdefine.}: string = ""
+{.passL: &"{ModBase}/cpp.o -lstdc++".}
+
+type
+  Player = distinct pointer
+  ServerNetworkHandler = distinct pointer
+
+var blacklist = initSet[string](64)
+let path_blacklist = getCurrentDir() / "games" / "blacklist.txt"
+
+proc checkRange(inStr: string, a, b: int): bool =
+  for i in a..<b:
+    if inStr[i] notin '0'..'9' and inStr[i] notin 'a'..'z': return false
+  true
+
+proc checkUUID(inStr: string): bool =
+  for i in [8, 13, 18, 23]:
+    if inStr[i] != '-': return false
+  checkRange(inStr, 0, 8) and checkRange(inStr, 9, 13) and checkRange(inStr, 14, 18) and checkRange(inStr, 19, 23) and checkRange(inStr, 24, 36)
+
+proc readBlacklist() =
+  try:
+    blacklist.clear
+    for token in lines(path_blacklist):
+      if token.len >= 36:
+        if checkUUID(token):
+          blacklist.incl(token[0..<36])
+          echo "§2[Blacklist Mod] Added <" & token[0..<36] & ">"
+        else:
+          echo "§4[Blacklist Mod] Invalid UUID: " & token
+    echo "§2[Blacklist Mod] Loaded " & $blacklist.len & " UUID."
+  except IOError:
+    echo("§4[Blacklist Mod] §kFailed to load blacklist(", path_blacklist, ").")
+
+var
+  handler: ServerNetworkHandler = nil
+
+proc setupCommands(registry: pointer) {.importc.}
+
+proc addToBlacklist(snh: ServerNetworkHandler, uuid: pointer, reason: ptr cstring) {.importc:"_ZN20ServerNetworkHandler14addToBlacklistERKN3mce4UUIDERKSs".}
+proc removeFromBlacklist(snh: ServerNetworkHandler, uuid: pointer, reason: ptr cstring) {.importc:"_ZN20ServerNetworkHandler19removeFromBlacklistERKN3mce4UUIDERKSs".}
+
+proc kickPlayer(snh: ServerNetworkHandler, p: Player) {.importc:"_ZN20ServerNetworkHandler13_onPlayerLeftEP12ServerPlayer".}
+
+proc showUuid(ba: array[0x10, byte]): string {.noSideEffect.} =
+  const hexChars = "0123456789abcdef"
+
+  result = newString(36)
+  for i in 0..<4:
+    result[2*i] = hexChars[int ba[7-i] shr 4 and 0xF]
+    result[2*i+1] = hexChars[int ba[7-i] and 0xF]
+  result[8] = '-'
+  for i in 4..<6:
+    result[2*i+1] = hexChars[int ba[7-i] shr 4 and 0xF]
+    result[2*i+2] = hexChars[int ba[7-i] and 0xF]
+  result[13] = '-'
+  for i in 6..<8:
+    result[2*i+2] = hexChars[int ba[7-i] shr 4 and 0xF]
+    result[2*i+3] = hexChars[int ba[7-i] and 0xF]
+  result[18] = '-'
+  for i in 8..<10:
+    result[2*i+3] = hexChars[int ba[23-i] shr 4 and 0xF]
+    result[2*i+4] = hexChars[int ba[23-i] and 0xF]
+  result[23] = '-'
+  for i in 10..<16:
+    result[2*i+4] = hexChars[int ba[23-i] shr 4 and 0xF]
+    result[2*i+5] = hexChars[int ba[23-i] and 0xF]
+
+proc banPlayer(uuid: ptr array[0x10, byte], reason: ptr cstring) {.exportc.} =
+  handler.addToBlacklist(uuid, reason)
+  try:
+    let stream = newFileStream(path_blacklist, fmAppend)
+    defer: stream.close()
+    stream.writeLine(uuid[].showUuid)
+  except:
+    echo "§4[Blacklist Mod] Blacklist Not Saved"
+
+proc pardonPlayer(uuid: ptr array[0x10, byte], reason: ptr cstring) {.exportc.} =
+  handler.removeFromBlacklist(uuid, reason)
+  let uuitStr = uuid[].showUuid
+  try:
+    let file = open(path_blacklist, fmRead)
+    defer: close(file)
+    let contents = readAll(file)
+    let stream = newFileStream(path_blacklist, fmWrite)
+    defer: close(stream)
+    for line in splitLines(contents):
+      if not line.startsWith(uuitStr):
+        stream.writeLine(line)
+  except:
+    echo "§4[Blacklist Mod] Blacklist Not Saved"
+
+proc kickPlayer(p: Player) {. cdecl, exportc .} =
+  handler.kickPlayer(p)
+
+proc createUUID(str: cstring): pointer {.importc.}
+
+proc bannedStr(): ptr cstring {.importc.}
+
+var isFirst = true
+
+hook "_ZN20ServerNetworkHandler24updateServerAnnouncementEv":
+  proc setSNH(snh: ServerNetworkHandler) {.refl.} =
+    if isFirst:
+      isFirst = false
+      handler = snh
+      readBlacklist()
+      for item in blacklist:
+        handler.addToBlacklist(createUUID(item), bannedStr())
+
+hook "_ZN10SayCommand5setupER15CommandRegistry":
+  proc setupCommand(registry: pointer) {.refl.} =
+    setupCommands(registry)
+
+proc mod_init(): void {. cdecl, exportc .} =
+  echo "Blacklist Mod Loaded"
