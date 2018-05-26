7e57c4b0032629f9c26871018b3202fe30d63c4f
CodeHz <CodeHz@outlook.com>
2018-05-26 22:00:27 +0800

    Only apply lock for shulkerbox

--- src/chest_lock/main.nim
+++ src/chest_lock/main.nim
@@ -4,7 +4,7 @@ const ModBase {.strdefine.}: string = ""
 
 type
   Player = distinct pointer
-  BlockEntity = distinct pointer
+  BlockEntity = distinct ptr ptr pointer
   BlockPos = distinct pointer
 
 proc getSelectedName(player: Player): cstring {.importc.}
@@ -13,6 +13,10 @@ proc getBlockEntityName(entity: BlockEntity): cstring {.importc.}
 proc sendMessage(player, packet: pointer) {.importc: "_ZNK12ServerPlayer17sendNetworkPacketER6Packet".}
 proc ServerPlayer_sendNetworkPacket(player, packet: pointer) {.exportc.} = sendMessage(player, packet)
 proc sendSystemMessage(player: Player, msg: cstring) {.importc.}
+proc shulkerBoxD2() {.importc:"_ZN21ShulkerBoxBlockEntityD2Ev".}
+
+proc checkShulkerBox(target: BlockEntity): bool =
+  return ((ptr ptr pointer)target)[][] == shulkerBoxD2
 
 proc checkAccess(player: Player, pos: BlockPos): bool =
   let chestName = player.getBlockEntity(pos).getBlockEntityName()
@@ -26,13 +30,13 @@ proc checkAccess(player: Player, pos: BlockPos): bool =
       return false
   return true
 
-hook "_ZNK11EntityBlock17playerWillDestroyER6PlayerRK8BlockPosRK5Block":
-  proc playerWillDestroy(entity: pointer, player: Player, pos: BlockPos, blk: pointer): pointer {.refl.} =
+hook "_ZNK15ShulkerBoxBlock17playerWillDestroyER6PlayerRK8BlockPosRK5Block":
+  proc playerWillDestroy(entity: BlockEntity, player: Player, pos: BlockPos, blk: pointer): pointer {.refl.} =
     if not checkAccess(player, pos): return nil
 
 hook "_ZNK10ChestBlock3useER6PlayerRK8BlockPosP15ItemUseCallback":
-  proc useChest(chest: pointer, player: Player, pos: BlockPos, callback: pointer): pointer {.refl.} =
-    if not checkAccess(player, pos): return nil
+  proc useChest(chest: BlockEntity, player: Player, pos: BlockPos, callback: pointer): pointer {.refl.} =
+    if getBlockEntity(player, pos).checkShulkerBox and not checkAccess(player, pos): return nil
 
 proc mod_init(): void {. cdecl, exportc .} =
   echo "Chest Lock Loaded"
